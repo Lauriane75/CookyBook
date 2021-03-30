c4c94444f0a28acf0b64e683a8a531821d207dd0
Lauriane <lauriane.haydari@gmail.com>
2021-03-30 15:23:41 +0200

    perform changes ios14

---
+++ cooky-book/App/Modules/Home/Managers/DataBaseManager.swift
@@ -0,0 +1,9 @@
+//
+//  DataBaseManager.swift
+//  cooky-book
+//
+//  Created by Lauriane Haydari on 07/12/2020.
+//  Copyright © 2020 Lauriane Haydari. All rights reserved.
+//
+
+import Foundation
